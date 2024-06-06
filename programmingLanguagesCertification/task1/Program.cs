2b49a82d2de0dc5fee7e4eec5521eea48a74f31a
Nikolay Litvinchuk <nikolajlitvinchuk1985@gmail.com>
2024-06-07 01:26:31 +0700

    addCertification:task1

---
+++ programmingLanguagesCertification/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+// Пример:
+// m = 1, n = 5   =>   "1, 2, 3, 4, 5"
+// m = 4, n = 8   =>   "4, 5, 6, 7, 8"
+// m = 8, n = 4   =>   "8, 7, 6, 5, 4"
+
+void PrintNaturalNumbers(int n, int m)
+{
+    if (n < m)
+    {
+        if (n > m) return;
+        Console.Write($"{n} ");
+        PrintNaturalNumbers(n + 1, m - 0);
+    }
+    else if (n > m)
+    {
+        if (n < m) return;
+        Console.Write($"{n} ");
+        PrintNaturalNumbers(n - 1, m - 0);
+    }
+
+}
+
+PrintNaturalNumbers(-5, -1);
+
