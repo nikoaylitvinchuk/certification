d5816b671ab23c5b7a76b39f845cf1910f7de7d1
Nikolay Litvinchuk <nikolajlitvinchuk1985@gmail.com>
2024-06-09 14:41:17 +0700

    addCertification:task3

---
+++ programmingLanguagesCertification/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте произвольный массив. Выведете его элементы,
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+// Пример:
+// [1, 2, 5, 0, 10, 34]   =>   34 10 0 5 2 1
+
+void PrintCoupArrayRecursion(int[] arr, int index)
+{
+    if (index >= 0)
+    {
+    Console.Write(arr[index] + " ");
+    PrintCoupArrayRecursion(arr, index - 1);
+    }
+}
+
+int[] normalArray = {1, 2, 5, 0, 10, 34};
+PrintCoupArrayRecursion(normalArray, normalArray.Length - 1);
